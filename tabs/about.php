a6a2ca0cf577bd7a5ae3ccec48a7c43e8d1f8fab
David Shorthouse <davidpshorthouse@gmail.com>
2011-11-03 13:15:16 -0400

    Fixed html, css for about.php

--- tabs/about.php
+++ tabs/about.php
@@ -9,25 +9,27 @@ require_once('../config/conf.php');
 <div class="header"><h2><?php echo _("Citing"); ?></h2></div>
 <p>Shorthouse, David P. 2010. SimpleMappr, a web-enabled tool to produce publication-quality point maps. Retrieved from http://www.simplemappr.net. Accessed <?php echo date("Y-m-d"); ?>.</p>
 <div class="header"><h2><?php echo _("Recent Updates"); ?></h2></div>
-<p class="update"><strong>October 24, 2011</strong><span>Bug fixes: corrected mechanism to draw map borders to accommodate rotation.</span></p>
-<p class="update"><strong>October 23, 2011</strong><span>Improvements: converted map rotation to a circular slider; loading a saved map with a rotation will indicate rotated angle.</span></p>
-<p class="update"><strong>October 20, 2011</strong><span>Improvements: added a Country label option; scale bar is now always present in preview but optional for download.</span></p>
-<p class="update"><strong>October 19, 2011</strong><span>Improvements: legend appearance in downloads and API. Bug fixes: proper rendering of Point Data/Regions layers after one or more were removed; improved behaviour of crop when map loaded.</span></p>
-<p class="update"><strong>October 18, 2011</strong><span>Improvements: dedicated label section in map preview settings; State/Province borders thickened to differentiate from rivers and lake outlines; lake fills made less stark to accommodate lake and river labels.</span></p>
-<p class="update"><strong>October 6, 2011</strong><span>Improvements: removed eps as an output format because it was raster-based; preview of shaded relief layers made faster.</span></p>
-<p class="update"><strong>October 2, 2011</strong><span>Bug fixes: incorrect expansion of Point Data/Regions layers after a saved map is loaded (reported by Chris Borkent).</span></p>
-<p class="update"><strong>October 1, 2011</strong><span>Improvements: keyboard shortcuts for common actions (arrow keys pan when cursor is hovered over the map).</span></p>
-<p class="update"><strong>September 29, 2011</strong><span>Improvements: crop selection restored after refreshing, zooming, panning, or selecting different layers or options.</span></p>
-<p class="update"><strong>September 28, 2011</strong><span>Improvements: editable corner coordinates for crop and improved accuracy.</span></p>
-<p class="update"><strong>September 26, 2011</strong><span>Improvements: responsiveness of application improved by accommodating browser caching.</span></p>
-<p class="update"><strong>September 21, 2011</strong><span>Improvements: added graticule options (requested by Jeff Skevington); cleaned presentation of download options. Bug fixes: production of KML files.</span></p>
-<p class="update"><strong>August 2, 2011</strong><span>Improvements: refined error-handling in coordinate recognition.</span></p>
-<p class="update"><strong>July 5, 2011</strong><span>Improvements: filtering My Maps by title.</span></p>
-<p class="update"><strong>July 4, 2011</strong><span>Improvements: fill bucket produces a colour selector and regions may be immediately filled by either clicking or click-dragging on the map; repeat actions adds another layer to the Regions tab; clear buttons added to each layer in the Point Data and Regions tabs (requested by Chris Borkent).</span></p>
-<p class="update"><strong>July 3, 2011</strong><span>Bug fixes: removed State/Province line artifacts when map is reprojected (reported by Chris Borkent).</span></p>
-<p class="update"><strong>June 28, 2011</strong><span>Improvements: layers on the Point Data or Regions tabs may be removed (requested by Chris Borkent).</span></p>
-<p class="update"><strong>June 27, 2011</strong><span>Improvements: ISO Country codes and regions code table added to the Help tab (requested by API users).</span></p>
-<p class="update"><strong>June 26, 2011</strong><span>Improvements: file names may be specified when downloading.</span></p>
+<dl>
+<dt class="update">October 24, 2011</dt><dd>Bug fixes: corrected mechanism to draw map borders to accommodate rotation.</dd>
+<dt class="update">October 23, 2011</dt><dd>Improvements: converted map rotation to a circular slider; loading a saved map with a rotation will indicate rotated angle.</dd>
+<dt class="update">October 20, 2011</dt><dd>Improvements: added a Country label option; scale bar is now always present in preview but optional for download.</dd>
+<dt class="update">October 19, 2011</dt><dd>Improvements: legend appearance in downloads and API. Bug fixes: proper rendering of Point Data/Regions layers after one or more were removed; improved behaviour of crop when map loaded.</dd>
+<dt class="update">October 18, 2011</dt><dd>Improvements: dedicated label section in map preview settings; State/Province borders thickened to differentiate from rivers and lake outlines; lake fills made less stark to accommodate lake and river labels.</dd>
+<dt class="update">October 6, 2011</dt><dd>Improvements: removed eps as an output format because it was raster-based; preview of shaded relief layers made faster.</dd>
+<dt class="update">October 2, 2011</dt><dd>Bug fixes: incorrect expansion of Point Data/Regions layers after a saved map is loaded (reported by Chris Borkent).</dd>
+<dt class="update">October 1, 2011</dt><dd>Improvements: keyboard shortcuts for common actions (arrow keys pan when cursor is hovered over the map).</dd>
+<dt class="update">September 29, 2011</dt><dd>Improvements: crop selection restored after refreshing, zooming, panning, or selecting different layers or options.</dd>
+<dt class="update">September 28, 2011</dt><dd>Improvements: editable corner coordinates for crop and improved accuracy.</dd>
+<dt class="update">September 26, 2011</dt><dd>Improvements: responsiveness of application improved by accommodating browser caching.</dd>
+<dt class="update">September 21, 2011</dt><dd>Improvements: added graticule options (requested by Jeff Skevington); cleaned presentation of download options. Bug fixes: production of KML files.</dd>
+<dt class="update">August 2, 2011</dt><dd>Improvements: refined error-handling in coordinate recognition.</dd>
+<dt class="update">July 5, 2011</dt><dd>Improvements: filtering My Maps by title.</dd>
+<dt class="update">July 4, 2011</dt><dd>Improvements: fill bucket produces a colour selector and regions may be immediately filled by either clicking or click-dragging on the map; repeat actions adds another layer to the Regions tab; clear buttons added to each layer in the Point Data and Regions tabs (requested by Chris Borkent).</dd>
+<dt class="update">July 3, 2011</dt><dd>Bug fixes: removed State/Province line artifacts when map is reprojected (reported by Chris Borkent).</dd>
+<dt class="update">June 28, 2011</dt><dd>Improvements: layers on the Point Data or Regions tabs may be removed (requested by Chris Borkent).</dd>
+<dt class="update">June 27, 2011</dt><dd>Improvements: ISO Country codes and regions code table added to the Help tab (requested by API users).</dd>
+<dt class="update">June 26, 2011</dt><dd>Improvements: file names may be specified when downloading.</dd>
+</dl>
 <div class="header"><h2>In the Wild</h2></div>
 <p class="citation">Carr, Christina May. 2011. Polychaete diversity and distribution patterns in Canadian marine waters. <em>Marine Biodiversity</em> Online first, doi:<a href="http://dx.doi.org/10.1007/s12526-011-0095-y">10.1007/s12526-011-0095-y</a></p>
 <p class="citation">Carr, C.M., Hardy, S.M., Brown, T.M., Macdonald, T.A., Hebert, P.D.N. 2011. A Tri-Oceanic Perspective: DNA Barcoding Reveals Geographic Structure and Cryptic Diversity in Canadian Polychaetes. <em>PLoS ONE</em> 6(7): e22232. doi:<a href="http://dx.doi.org/10.1371/journal.pone.0022232">10.1371/journal.pone.0022232</a></p>
