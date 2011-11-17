4f04cb7e9fd423d1c34542c22e3c7cc26f601644
David Shorthouse <davidpshorthouse@gmail.com>
2011-11-17 08:26:02 -0500

    Recreated .po files, fixed directory name for ES, stripped chat option

--- tabs/feedback.php
+++ tabs/feedback.php
@@ -5,9 +5,10 @@
 <!-- Disqus BEGIN -->
 <div id="disqus_thread"></div>
 <script type="text/javascript">
+  var disqus_shortname = 'simplemappr';
   (function() {
    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-   dsq.src = 'http://simplemappr.disqus.com/embed.js';
+   dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   })();
 </script>
