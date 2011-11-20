4ad27de6aba01b3acea9e3f96603642e0068c17a
David Shorthouse <davidpshorthouse@gmail.com>
2011-11-20 18:23:28 -0500

    Modified jquery.download extension to prevent script errors with large data POST

--- public/javascript/jquery.download.js
+++ public/javascript/jquery.download.js
@@ -1,19 +1,19 @@
 /*global jQuery, unescape */
 
-jQuery.download = function(url, data, method) {
+jQuery.download = function(url, data, method){
 
   "use strict";
 
-  var inputs = '', pair = [], value = "";
+  var form = '', id = '', pair = [];
 
-  if( url && data ) { 
+  if(url && data){
     data = (typeof data === 'string') ? data : jQuery.param(data);
-    jQuery.each(data.split('&'), function() { 
+    form = jQuery('<form action="' + url + '" method="' + (method||'post') + '"></form>');
+    jQuery.each(data.split('&'), function(){
       pair = this.split('=');
-      value = pair[1].replace(/\+/g,' ');
-      inputs+='<input type="hidden" name="'+ unescape(pair[0]) +'" value="'+ unescape(value) +'" />';
+      id = 'jquery-download-' + unescape(pair[0]);
+      form.append('<input type="hidden" name="' + unescape(pair[0]) + '" id="' + id + '" value="' + unescape(pair[1].replace(/\+/g,' ')) + '" />');
     });
-    //send request
-    jQuery('<form action="'+ url +'" method="'+ (method||'post') +'">'+inputs+'</form>').appendTo('body').submit().remove();
+    form.appendTo('body').submit().remove();
   }
-};
+}; 
