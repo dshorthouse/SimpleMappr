63c7983aa5c475198e55cbbfdc2b9e1cb35c3c42
David Shorthouse <davidpshorthouse@gmail.com>
2012-01-05 18:21:11 -0500

    Not ideal, but remove async js on feedback tab

--- tabs/feedback.php
+++ tabs/feedback.php
@@ -19,23 +19,26 @@ $tweet = ($lang['canonical'] == 'en') ? 'Tweet' : 'Tweeter';
 <script type="text/javascript">
   var disqus_shortname = 'simplemappr',
   disqus_config = function() { this.language = "<?php echo $lang['canonical']; ?>"; };
+  function remove(id) {
+    return (elem=document.getElementById(id)).parentNode.removeChild(elem);
+  }
   (function(d, s, id) {
    var js, djs=d.getElementsByTagName(s)[0];
-   if(d.getElementById(id)) return;
+   if(d.getElementById(id)) { remove(id); }
    js=d.createElement(s); js.id=id;
    js.src='//' + disqus_shortname + '.disqus.com/embed.js';
    djs.parentNode.insertBefore(js, djs);
   })(document, 'script', 'disqus-wjs');
   (function(d, s, id) {
     var js, fjs=d.getElementsByTagName(s)[0];
-    if(d.getElementById(id)) return;
+    if(d.getElementById(id)) { remove(id); }
     js=d.createElement(s); js.id=id;
     js.src="//connect.facebook.net/<?php echo $lang['locale']; ?>/all.js#xfbml=1";
     fjs.parentNode.insertBefore(js, fjs);
   })(document, 'script', 'facebook-jssdk');
   (function(d, s, id) {
     var js, fjs=d.getElementsByTagName(s)[0];
-    if(d.getElementById(id)) return;
+    if(d.getElementById(id)) { remove(id); }
     js=d.createElement(s); js.id=id;
     js.src="//platform.twitter.com/widgets.js";
     fjs.parentNode.insertBefore(js,fjs);
