6cc2e737e7e4019ee747568d53217182de7f806f
dshorthouse <davidpshorthouse@gmail.com>
2012-10-14 21:06:41 -0400

    Adjusted API documentation

--- tabs/api.php
+++ tabs/api.php
@@ -46,7 +46,8 @@ USERSESSION::select_locale();
     </dd>
 
     <dt>file</dt>
-    <dd><?php echo _("a tab-separated text file the columns within which are treated as groups of points; the first row used for an optional legend; rows are comma- or space-separated. Your POST script must set the enctype to multipart/form-data. If a file is sent, the initial response will be JSON with an imageURL element and an expiry element, which indicates when the file will likely be deleted from the server."); ?>
+    <dd><?php echo sprintf(_("Note: requires a POST request to http://%s/api/ with an enctype set to multipart/form-data."), $_SERVER['HTTP_HOST']) ?><br />
+        <?php echo _("tab-separated text file the columns within which are treated as groups of points; the first row used for an optional legend; rows are comma- or space-separated. The initial response will be JSON with an imageURL element and an expiry element, which indicates when the file will likely be deleted from the server."); ?>
     <span class="api-example"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/api/demo.txt">Example file</a></span>
     <span class="api-example"><em>e.g.</em> { "imageURL" : "http://<?php echo $_SERVER['HTTP_HOST'] ?>/tmp/50778960_464f_0.png", "expiry" : "<?php echo date('c', time() + (6 * 60 * 60)); ?>" }</span>
     </dd>
