a748fb3e2bb9f8004dfeebb7a456c31144ccef1d
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-05 01:14:32 -0400

    Make tests pass

--- Tests/MapprTest.php
+++ Tests/MapprTest.php
@@ -10,11 +10,21 @@ class MapprTest extends DatabaseTest {
     public function tearDown() {
     }
     
+    public function test_add_slashes_extended() {
+      
+    }
+    
+    public function test_clean_filename() {
+      $name = "My %!  <>  .  Map";
+      $clean = Mappr::clean_filename($name);
+      $this->assertEquals($clean, "My_Map");
+    }
+    
     public function test_make_coordinates() {
       $coord = '52° 32\' 25" N, 89° 40\' 31" W';
       $dd = Mappr::make_coordinates($coord);
       $this->assertEquals($dd[0], 52.540277777778);
-      $this->assertEquals($dd[1], 0);
+      $this->assertEquals($dd[1], -89.675277777778);
     }
 
 }
