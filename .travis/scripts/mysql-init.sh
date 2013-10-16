8663e3190b941ce872ee056f9378bfbf7d6a80f6
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-16 11:42:23 -0400

    Fix bash and add one for MySQL

---
+++ .travis/scripts/mysql-init.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DBNAME="simplemappr_test"
+if [ "$1" ]
+then
+    DBNAME="$1"
+fi
+
+USERNAME="root"
+if [ "$2" ]
+then
+    USERNAME="$2"
+fi
+
+PASSWORD=""
+if [ "$3" ]
+then
+    PASSWORD="-p$3"
+fi
+
+HASPASSWORD="without password"
+if [ "$PASSWORD" ]
+then
+    HASPASSWORD="with password"
+fi
+
+echo "---> Creating $(tput bold ; tput setaf 2)MySQL database$(tput sgr0) : $(tput bold ; tput setaf 3)$DBNAME$(tput sgr0)"
+echo "---> User $(tput bold ; tput setaf 2)$USERNAME ($HASPASSWORD)$(tput sgr0)"
+
+mysql -u$USERNAME $PASSWORD -e "DROP DATABASE IF EXISTS $DBNAME; CREATE DATABASE $DBNAME;"
