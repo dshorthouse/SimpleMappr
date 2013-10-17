9c60b868b0d82e18fddd59c9c16341527814d60c
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-17 15:24:42 -0400

    Add comments to simpletest output

--- Tests/MapprTest.php
+++ Tests/MapprTest.php
@@ -24,17 +24,21 @@ class MapprTest extends WebTestCase {
   }
 
   public function test_get_json_response() {
+    echo "----> Testing GET on " . WEB_SERVER . "/application" . "\n";
     $response = $this->get(WEB_SERVER . "/application");
     $this->assertMime("application/json");
   }
 
   public function test_post_json_response() {
+    echo "----> Testing POST on " . WEB_SERVER . "/application" . "\n";
     $response = $this->post(WEB_SERVER . "/application", array());
     $this->assertMime("application/json");
   }
 
   public function test_width() {
-    $response = $this->post(WEB_SERVER . "/application", array('width' => '300'));
+    $post = array('width' => '300');
+    echo "----> Testing POST on " . WEB_SERVER . "/application" . " with " . json_encode($post) . "\n";
+    $response = $this->post(WEB_SERVER . "/application", $post);
     $json = json_decode($response);
     $this->assertEqual($json->size[0], 300);
     $this->assertEqual($json->size[1], 150);
