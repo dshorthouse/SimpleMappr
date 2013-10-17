b5de6ea792bcb378da33ff386e6f67212753c88d
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-17 10:42:31 -0400

    Try fixing travis config

--- .travis/scripts/apache2-vhost.sh
+++ .travis/scripts/apache2-vhost.sh
@@ -4,30 +4,37 @@ BASEDIR=$(dirname $0)
 BASEDIR=$(readlink -f "$BASEDIR/..")
 ROOTDIR=$(readlink -f "$BASEDIR/..")
 
-VHOSTNAME="simplemappr.local"
+VHOSTNAME="www.simplemappr.local"
 if [ "$1" ]
 then
     VHOSTNAME="$1"
 fi
 
-DOCROOT="$ROOTDIR"
+ALIAS="simplemappr.local"
 if [ "$2" ]
 then
-    DOCROOT="$2"
+    ALIAS="$2"
+fi
+
+DOCROOT="$ROOTDIR"
+if [ "$3" ]
+then
+    DOCROOT="$3"
 fi
 
 CONFIGFILE="$BASEDIR/apache2/virtualhost.local-dist"
-if [ "$3" ]
+if [ "$4" ]
 then
-    CONFIGFILE="$3"
+    CONFIGFILE="$4"
 fi
 
 echo "---> Starting $(tput bold ; tput setaf 2)virtual host creation$(tput sgr0)"
 echo "---> Virtualhost name : $(tput bold ; tput setaf 3)$VHOSTNAME$(tput sgr0)"
+echo "---> Alias name: $(tput bold ; tput setaf 3)$ALIAS$(tput sgr0)"
 echo "---> Document root : $(tput bold ; tput setaf 3)$DOCROOT$(tput sgr0)"
 echo "---> Configuration file : $(tput bold ; tput setaf 3)$CONFIGFILE$(tput sgr0)"
 
-sed s?%basedir%?$DOCROOT? "$CONFIGFILE" | sed s/%hostname%/$VHOSTNAME/ > $VHOSTNAME
+sed s?%basedir%?$DOCROOT? "$CONFIGFILE" | sed s/%hostname%/$VHOSTNAME/ | sed s/%alias%/$ALIAS/ > $VHOSTNAME
 sudo mv $VHOSTNAME /etc/apache2/sites-available/$VHOSTNAME
 
 echo "---> $(tput bold ; tput setaf 2)Adding host to /etc/hosts$(tput sgr0) :"
