803f8c58db5200fb9fe6c8738dd24962d3c2597a
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-21 21:15:55 -0400

    Add base shapefile to travis test

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -9,5 +9,9 @@ tar -zxvf mapserver-6.4.0.tar.gz
 cmake mapserver-6.4.0 -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=0
 make
 sudo make install
-mapserv -v
-sudo service apache2 restart
+
+echo "---> Download map files"
+mkdir lib/mapserver/maps/10m_cultural/10m_cultural/
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip
+unzip ne_10m_admin_0_map_units.zip -d lib/mapserver/maps/10m_cultural/10m_cultural/
+mv lib/mapserver/maps/10m_cultural/10m_cultural/ne_10m_admin_0_map_units/* lib/mapserver/maps/10m_cultural/10m_cultural/
