feb6a6b20cf404a27ba1bfcf1ef5b781a4fcbf9c
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-21 21:52:10 -0400

    Silly mistake with mkdir

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -11,7 +11,7 @@ make
 sudo make install
 
 echo "---> Download map files"
-mkdir lib/mapserver/maps/10m_cultural/10m_cultural/
+mkdir -p lib/mapserver/maps/10m_cultural/10m_cultural/
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip
 unzip ne_10m_admin_0_map_units.zip -d lib/mapserver/maps/10m_cultural/10m_cultural/
 mv lib/mapserver/maps/10m_cultural/10m_cultural/ne_10m_admin_0_map_units/* lib/mapserver/maps/10m_cultural/10m_cultural/
--- Tests/MapprApplicationTest.php
+++ Tests/MapprApplicationTest.php
@@ -27,14 +27,14 @@ class MapprApplicationTest extends WebTestCase {
   public function test_get_json_response() {
     echo "----> Testing GET on " . $this->app_url . "\n";
     echo "Response:" . "\n";
-    echo $this->get($this->app_url) . "\n";
+    echo $this->get($this->app_url) . "\n\n";
     $this->assertMime("application/json");
   }
 
   public function test_post_json_response() {
     echo "----> Testing POST on " . $this->app_url . "\n";
     echo "Response:" . "\n";
-    echo $this->post($this->app_url, array()) . "\n";
+    echo $this->post($this->app_url, array()) . "\n\n";
     $this->assertMime("application/json");
   }
 
