d84c3734563d607297d06af8984d0df5648a12b9
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-22 10:28:37 -0400

    Attempt toconfigure locales

--- .travis/scripts/translations.sh
+++ .travis/scripts/translations.sh
@@ -14,6 +14,8 @@ echo "---> Generating $(tput bold ; tput setaf 2)locales$(tput sgr0)"
 sudo locale-gen
 sudo locale-gen en_US.UTF-8
 sudo locale-gen fr_FR.UTF-8
+sudo update-locale
+sudo dpkg-reconfigure locales
 
 echo "---> Translating $(tput bold ; tput setaf 3)$DOCROOT$/i18n/fr_FR.UTF-8/LC_MESSAGES/messages.po(tput sgr0)"
 msgfmt $DOCROOT/i18n/fr_FR.UTF-8/LC_MESSAGES/messages.po
