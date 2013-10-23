d93970a09f4fb9489313107e755bec6ca4f4d5b6
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-22 20:26:49 -0400

    Attempt #2 for Selenium

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -6,7 +6,7 @@ pear install phpunit/DbUnit
 pear install phpunit/PHPUnit_Selenium
 pear install phpunit/PHPUnit_Story
 
-/etc/init.d/xvfb start
+sudo /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.9.0.jar
 java -jar selenium-server-standalone-2.9.0.jar > /dev/null &
