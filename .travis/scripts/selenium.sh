c83d4a82ae65841ab260b7d2322f5a95b778550b
David Shorthouse <davidpshorthouse@gmail.com>
2013-10-22 20:21:21 -0400

    Attempt #2 for Selenium

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -1,32 +1,13 @@
 #!/bin/sh
 
-serverUrl='http://127.0.0.1:4444'
-serverFile=selenium-server-standalone-2.9.0.jar
-firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.0/linux-x86_64/en-US/firefox-24.0.tar.bz2
-firefoxFile=firefox.tar.bz2
-
 pear channel-discover pear.phpunit.de
 pear install phpunit/PHP_Invoker
 pear install phpunit/DbUnit
 pear install phpunit/PHPUnit_Selenium
 pear install phpunit/PHPUnit_Story
 
-echo "---> Downloading Firefox..."
-wget $firefoxUrl -O $firefoxFile
-tar xvjf $firefoxFile
-
-echo "---> Starting xvfb..."
-echo "---> Starting Selenium..."
-
-if [ ! -f $serverFile ]; then
-    wget http://selenium.googlecode.com/files/$serverFile
-fi
-
-sudo xvfb-run java -jar $serverFile > /tmp/selenium.log &
-wget --retry-connrefused --tries=60 --waitretry=1 --output-file=/dev/null $serverUrl/wd/hub/status -O /dev/null
-
-if [ ! $? -eq 0 ]; then
-    echo "Selenium Server not started"
-else
-    echo "Finished setup"
-fi
+/etc/init.d/xvfb start
+export DISPLAY=:99.0
+wget http://selenium.googlecode.com/files/selenium-server-standalone-2.9.0.jar
+java -jar selenium-server-standalone-2.9.0.jar > /dev/null &
+sleep 10
