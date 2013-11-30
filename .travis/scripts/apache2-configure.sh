c29b19beca4920feee84a5c0359b67e182aeffa6
David Shorthouse <davidpshorthouse@gmail.com>
2013-11-30 12:07:39 -0500

    Forgot to include apache module

--- .travis/scripts/apache2-configure.sh
+++ .travis/scripts/apache2-configure.sh
@@ -2,4 +2,4 @@
 
 echo "---> Applying $(tput bold ; tput setaf 2)apache2 configuration$(tput sgr0)"
 
-sudo a2enmod rewrite expires headers deflate alias fastcgi
+sudo a2enmod actions rewrite expires headers deflate alias fastcgi
