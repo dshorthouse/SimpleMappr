f8073d72fcc0cc599c74c273511ae6aac59cacbc
David Shorthouse <davidpshorthouse@gmail.com>
2014-04-27 23:41:18 -0400

    Update inclusion of bash script for testing

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -11,16 +11,3 @@ cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=0 ..
 make
 sudo make install
 cd ../../
-
-echo "---> Download map files from Natural Earth"
-
-mkdir -p lib/mapserver/maps/10m_cultural/10m_cultural/
-mkdir -p lib/mapserver/maps/10m_physical/
-
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_lakes.zip
-
-unzip ne_10m_admin_0_map_units.zip -d lib/mapserver/maps/10m_cultural/10m_cultural/
-unzip ne_10m_admin_1_states_provinces.zip -d lib/mapserver/maps/10m_cultural/10m_cultural/
-unzip ne_10m_lakes.zip -d lib/mapserver/10m_physical/
