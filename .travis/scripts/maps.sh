36d36375bd5760306fcf41e975bbad9fb9e82c7b
David Shorthouse <davidpshorthouse@gmail.com>
2014-05-02 20:23:16 -0400

    Rearrange Travis config, use different shapefile for stateprovinces layer in tests

--- .travis/scripts/maps.sh
+++ .travis/scripts/maps.sh
@@ -6,7 +6,7 @@ mkdir -p lib/mapserver/maps/10m_cultural/10m_cultural/
 mkdir -p lib/mapserver/maps/10m_physical/
 
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces_lines.zip
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_lakes.zip
 
 unzip ne_10m_admin_0_map_units.zip -d lib/mapserver/maps/10m_cultural/10m_cultural/
