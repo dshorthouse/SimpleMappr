b1d49a43d184cdf0fabc7e290c5b28131a7f90e5
David Shorthouse <davidpshorthouse@gmail.com>
2014-12-18 16:02:20 -0500

    Fixed minor issue with translation

--- views/about.php
+++ views/about.php
@@ -27,7 +27,7 @@ Session::select_locale();
 <div id="recent-updates">
 <div class="header"><h2><?php echo _("Recent Updates"); ?></h2></div>
 <dl>
-<dt class="update"><?php echo _("December 17, 2014"); ?></dt><dd><?php echo ("Improvements: Added a Countries layer to toggle international borders. Requested by Yossef Rapoport.")?></dd>
+<dt class="update"><?php echo _("December 17, 2014"); ?></dt><dd><?php echo _("Improvements: Added a Countries layer to toggle international borders. Requested by Yossef Rapoport.")?></dd>
 <dt class="update"><?php echo _("December 4, 2014"); ?></dt><dd><?php echo _("Improvements: Added file upload for Point Layer data."); ?></dd>
 <dt class="update"><?php echo _("August 14, 2014"); ?></dt><dd><?php echo _("Improvements: Added an icon in the toolbar to create a new empty map."); ?></dd>
 <dt class="update"><?php echo _("July 27, 2014"); ?></dt><dd><?php echo _("Improvements: Recognize decimal degree coordinates with degree symbol."); ?></dd>
