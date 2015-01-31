07f3bfd01210f21c55163fa42de8a92c8edba933
David Shorthouse <davidpshorthouse@gmail.com>
2015-01-30 22:01:56 -0500

    Fix bug in css for error page

--- lib/Header.class.php
+++ lib/Header.class.php
@@ -360,10 +360,10 @@ class Header
                 fwrite($handle, $css_min);
                 fclose($handle);
 
-                $this->addCSS('<link type="text/css" href="public/stylesheets/cache/' . $css_min_file . '" rel="stylesheet" media="screen,print" />');
+                $this->addCSS('<link type="text/css" href="/public/stylesheets/cache/' . $css_min_file . '" rel="stylesheet" media="screen,print" />');
             } else {
                 foreach ($cached_css as $css) {
-                    $this->addCSS('<link type="text/css" href="public/stylesheets/cache/' . $css . '" rel="stylesheet" media="screen,print" />');
+                    $this->addCSS('<link type="text/css" href="/public/stylesheets/cache/' . $css . '" rel="stylesheet" media="screen,print" />');
                 }
             }
 
