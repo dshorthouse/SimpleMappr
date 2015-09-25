ff8f711da4c7a69453bff96f1a3381855e7ec155
David Shorthouse <davidpshorthouse@gmail.com>
2015-09-25 10:19:47 -0400

    Add apt-get update to pull google-chrome-stable in Travis testing

--- .travis/scripts/chrome.sh
+++ .travis/scripts/chrome.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 echo "---> Starting Chrome driver installation"
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+sudo apt-get update
+
 sudo apt-get -y install python python-pip
 sudo pip install selenium
-
-wget -c http://chromedriver.storage.googleapis.com/2.19/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo cp ./chromedriver /usr/bin/
-sudo chmod ugo+rx /usr/bin/chromedriver
 
 sudo apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4
 sudo apt-get -y install google-chrome-stable
@@ -23,6 +21,11 @@ Xvfb -ac :99 -screen 0 1280x1024x16 &
 export DISPLAY=:99
 
 echo "Starting Selenium in background..."
+wget -c http://chromedriver.storage.googleapis.com/2.19/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo cp ./chromedriver /usr/bin/
+sudo chmod ugo+rx /usr/bin/chromedriver
+
 wget http://goo.gl/yLJLZg
 java -jar selenium-server-standalone-2.47.1.jar -Dwebdriver.chrome.driver=/usr/bin/chromedriver > /dev/null &
 sleep 5
