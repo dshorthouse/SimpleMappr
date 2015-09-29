f20e77a24c9e8b35d8cd041addd09604e52354e1
David Shorthouse <davidpshorthouse@gmail.com>
2015-09-28 20:43:12 -0400

    Add context in file_get_contents for Travis tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -79,8 +79,10 @@ function loader()
     switchConf();
     requireFiles();
     flushCaches();
+    $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n"));
+    $context = stream_context_create($opts);
     ob_start();
-    file_get_contents("http://".MAPPR_DOMAIN);
+    file_get_contents("http://".MAPPR_DOMAIN, false, $context);
     new \SimpleMappr\Header;
 }
 
