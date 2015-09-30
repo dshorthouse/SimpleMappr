33d6471b49a840547835fb7cc95435e18d8fa81c
David Shorthouse <davidpshorthouse@gmail.com>
2015-09-30 15:50:44 -0400

    Remove reference to conf file, fxn does not exist anyway

--- .travis/scripts/apache2-configure.sh
+++ .travis/scripts/apache2-configure.sh
@@ -4,5 +4,5 @@ echo "---> Applying $(tput bold ; tput setaf 2)apache2 configuration$(tput sgr0)
 
 sudo a2enmod actions rewrite expires headers deflate alias fastcgi
 sudo touch /usr/lib/cgi-bin/php5.fcgi
-sudo a2enconf php5-fpm
-sudo service apache2 restart && sudo service php5-fpm restart
+sudo service apache2 restart
+sudo service php5-fpm restart
