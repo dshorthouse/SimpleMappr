6e65254b67e00a7d796c9131072c5354034c55bb
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-04 13:36:00 -0400

    Restart PHP-FCGI and Apache2 once, remove manual addition to hosts file

--- .travis/scripts/apache2-configure.sh
+++ .travis/scripts/apache2-configure.sh
@@ -4,5 +4,3 @@ echo "---> Applying $(tput bold ; tput setaf 2)apache2 configuration$(tput sgr0)
 
 sudo a2enmod actions rewrite expires headers deflate alias fastcgi
 sudo touch /usr/lib/cgi-bin/php5.fcgi
-sudo service php5-fpm restart
-sudo service apache2 restart
