da0bfb551b22610d7c1f103b043a0b5702e107a6
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-03 21:18:33 -0400

    Test POST reqs to /application.json

--- Tests/SimpleMapprMixin.php
+++ Tests/SimpleMapprMixin.php
@@ -65,4 +65,25 @@ trait SimpleMapprMixin
             ->setMaxExtent("-180,-90,180,90");
         return $class;
     }
+
+    public function httpPost($url, $params)
+    {
+        $postData = '';
+        foreach($params as $k => $v) {
+            $postData .= $k . '='.$v.'&'; 
+        }
+        rtrim($postData, '&');
+
+        $ch = curl_init();  
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_HEADER, false); 
+        curl_setopt($ch, CURLOPT_POST, count($postData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+        return $output;
+    }
 }
---
+++ Tests/functional/ApplicationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Unit tests for static methods and default set-up of MapprApplication class
+ *
+ * PHP Version 5.5
+ *
+ * @author  David P. Shorthouse <davidpshorthouse@gmail.com>
+ * @link    http://github.com/dshorthouse/SimpleMappr
+ * @license Copyright (C) 2013 David P. Shorthouse
+ *
+ */
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+    use SimpleMapprMixin;
+
+    /**
+     * Parent setUp function executed before each test.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Parent tearDown function executed after each test.
+     */
+    protected function tearDown()
+    {
+        $this->clearTmpFiles();
+    }
+
+    /**
+     * Test that empty lines are removed.
+     */
+    public function test_postRequest()
+    {
+        $fields = array();
+        $response = $this->httpPost("http://" . MAPPR_DOMAIN . "/application.json", $fields);
+        $decoded = json_decode($response, true);
+        $this->assertContains(MAPPR_MAPS_URL, $decoded["mapOutputImage"]);
+        $image = file_get_contents($decoded["mapOutputImage"]);
+        $this->assertEquals("\x89PNG\x0d\x0a\x1a\x0a",substr($image,0,8));
+    }
+
+}
