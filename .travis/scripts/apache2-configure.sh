42948e7ee85c5b42b4acb11c8ee8de97351c11d3
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-07 15:09:42 -0400

    Continue adjustment of apache2 config

--- .travis/scripts/apache2-configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "---> Applying $(tput bold ; tput setaf 2)apache2 configuration$(tput sgr0)"
-
-sudo a2enmod actions rewrite expires headers deflate alias fastcgi
-sudo touch /usr/lib/cgi-bin/php5.fcgi
