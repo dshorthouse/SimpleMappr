7540a26ebf17d149f8ee0fdedd9781fff0d572d1
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-07 01:15:38 -0400

    Restore previous and print out phpinfo

--- .travis/scripts/apache2-configure.sh
+++ .travis/scripts/apache2-configure.sh
@@ -2,6 +2,5 @@
 
 echo "---> Applying $(tput bold ; tput setaf 2)apache2 configuration$(tput sgr0)"
 
-sudo a2dismod php5
 sudo a2enmod actions rewrite expires headers deflate alias fastcgi
 sudo touch /usr/lib/cgi-bin/php5.fcgi
