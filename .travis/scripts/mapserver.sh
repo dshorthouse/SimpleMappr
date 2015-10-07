128e39054a37b339e879596e0617f249559befeb
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-07 01:33:16 -0400

    Enable fastcgi flag for mapserver compile

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -7,7 +7,7 @@ tar -zxvf mapserver-7.0.0.tar.gz
 cd mapserver-7.0.0
 mkdir build
 cd build
-cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=0 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 ..
+cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=1 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 ..
 make
 sudo make install
 cd ../../
