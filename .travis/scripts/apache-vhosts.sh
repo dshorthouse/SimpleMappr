bf664ea224760b0b24252d8cba31c51c49dbb342
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-07 22:37:10 -0400

    Fix a2ensite for travis build

--- .travis/scripts/apache-vhosts.sh
+++ .travis/scripts/apache-vhosts.sh
@@ -4,11 +4,11 @@ echo "---> Starting $(tput bold ; tput setaf 2)virtual host creation$(tput sgr0)
 
 sudo cp -f .travis/apache2/www_simplemappr_local.conf /etc/apache2/sites-available/www_simplemappr_local.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/www_simplemappr_local.conf
-sudo a2ensite "www.simplemappr.local"
+sudo a2ensite www_simplemappr_local.conf
 
 sudo cp -f .travis/apache2/img_simplemappr_local.conf /etc/apache2/sites-available/img_simplemappr_local.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/img_simplemappr_local.conf
-sudo a2ensite "img.simplemappr.local"
+sudo a2ensite www_simplemappr_local.conf
 
 sudo a2enmod actions rewrite expires headers
 sudo touch /usr/lib/cgi-bin/php5.fcgi
