e32386b2248e7e40a82e9dee30b691a487be30a3
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-08 00:05:33 -0400

    Try to resolve apparent permissions issues with php-fpm

--- .travis/scripts/permissions.sh
+++ .travis/scripts/permissions.sh
@@ -7,3 +7,5 @@ sudo chmod -R 777 public/stylesheets/cache/
 sudo chmod -R 777 config/
 sudo chmod -R 777 mapserver/
 sudo chmod -R 777 log/
+sudo chmod -R 666 /usr/lib/cgi-bin
+sudo chown -R www-data:www-data /usr/lib/cgi-bin
