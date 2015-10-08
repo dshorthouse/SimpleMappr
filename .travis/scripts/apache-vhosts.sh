83aaec0a1a5337cf7365516078708f1224224a81
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-08 00:53:44 -0400

    Fixed a2ensite

--- .travis/scripts/apache-vhosts.sh
+++ .travis/scripts/apache-vhosts.sh
@@ -8,7 +8,7 @@ sudo a2ensite www_simplemappr_local.conf
 
 sudo cp -f .travis/apache2/img_simplemappr_local.conf /etc/apache2/sites-available/img_simplemappr_local.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/img_simplemappr_local.conf
-sudo a2ensite www_simplemappr_local.conf
+sudo a2ensite img_simplemappr_local.conf
 
 sudo a2enmod actions rewrite expires headers
 
