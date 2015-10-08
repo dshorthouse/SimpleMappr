04e909851d1ab8fdd3043c4f81a9b9dff4046035
David Shorthouse <davidpshorthouse@gmail.com>
2015-10-07 21:34:59 -0400

    Recreate bash file for vhosts

---
+++ .travis/scripts/apache-vhosts.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "---> Starting $(tput bold ; tput setaf 2)virtual host creation$(tput sgr0)"
+
+sudo cp -f .travis/apache2/www_simplemappr_local.conf /etc/apache2/sites-available/www_simplemappr_local.conf
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/www_simplemappr_local.conf
+sudo a2ensite "www.simplemappr.local"
+
+sudo cp -f .travis/apache2/img_simplemappr_local.conf /etc/apache2/sites-available/img_simplemappr_local.conf
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/img_simplemappr_local.conf
+sudo a2ensite "img.simplemappr.local"
+
+sudo a2enmod actions rewrite expires headers
+sudo touch /usr/lib/cgi-bin/php5.fcgi
