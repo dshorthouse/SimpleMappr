65e55a79cff770173490a701c5c0bf81a0e0ee80
David Shorthouse <davidpshorthouse@gmail.com>
2015-12-03 16:02:18 -0500

    Clean unnecessary code from GA injection

--- src/Header.php
+++ src/Header.php
@@ -562,13 +562,12 @@ else if (w.onLoad) { w.onload = isJanrainReady; }
     {
         $analytics = "";
         if (ENVIRONMENT == "production" || ENVIRONMENT == "testing") {
-            $host = parse_url(MAPPR_URL);
             $analytics  = "<script>" . "\n";
             $analytics .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');" . "\n";
-            $analytics .= "ga('create', '".GOOGLE_ANALYTICS."', '".$host["host"]."');" . "\n";
+            $analytics .= "ga('create', '".GOOGLE_ANALYTICS."', '".Utilities::parsedURL()["host"]."');" . "\n";
             $analytics .= "ga('send', 'pageview');" . "\n";
             $analytics .= "</script>" . "\n";
         }
