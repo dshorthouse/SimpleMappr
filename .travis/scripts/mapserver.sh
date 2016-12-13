005d9908c3ed737a62949dd17848b3323a415f92
David Shorthouse <davidpshorthouse@gmail.com>
2016-12-13 11:57:11 -0500

    Add mapserver src files to Test dir

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -2,11 +2,9 @@
 
 echo "---> Starting MapServer 7.0.3 installation"
 
-wget http://download.osgeo.org/mapserver/mapserver-7.0.3.tar.gz
-tar -zxvf mapserver-7.0.3.tar.gz
-cd mapserver-7.0.3
-mkdir build
-cd build
+tar -zxvf Tests/files/mapserver-7.0.3.tar.gz -C .
+mkdir mapserver-7.0.3/build
+cd mapserver-7.0.3/build
 cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=1 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 ..
 make
 sudo make install
