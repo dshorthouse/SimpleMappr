270b8bea0ad475d42ae6793a582d397747ef29d7
David Shorthouse <davidpshorthouse@gmail.com>
2016-12-13 15:04:32 -0500

    Resume download of MapServer from source during Travis-CI execution

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-echo "---> Starting MapServer 7.0.3 installation"
+mapserver="mapserver-7.0.2"
 
-tar -zxvf Tests/files/mapserver-7.0.3.tar.gz -C .
-mkdir mapserver-7.0.3/build
-cd mapserver-7.0.3/build
+echo "---> Starting $mapserver installation"
+
+wget http://download.osgeo.org/mapserver/$mapserver.tar.gz
+tar -zxvf $mapserver.tar.gz
+mkdir $mapserver/build
+cd $mapserver/build
 cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=1 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 ..
 make
 sudo make install
 cd ../../
 mapserv -v
-#
-#search="MapScript"
-#mapscript="$(php -m | grep $search)"
-#
-#if [ "$mapscript" = "$search" ]; then
-#  echo $(tput bold ; tput setaf 2)$search installed$(tput sgr0)
-#else
-#  echo $(tput bold ; tput setaf 1)$search NOT installed$(tput sgr0)
-#fi
