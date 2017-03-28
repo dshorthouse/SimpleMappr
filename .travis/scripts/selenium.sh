ddde6fae4870ecf93ad39b9bbb464d46e1ec6cac
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-28 15:18:08 -0400

    Spit out gecko version on Travis-CI

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -4,9 +4,9 @@ wget "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standal
 sudo mv selenium-server-standalone-3.3.1.jar /usr/bin/selenium.jar
 
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz"
-mkdir geckodriver
-tar -zxf geckodriver-v0.15.0-linux64.tar.gz -C geckodriver
-export PATH=$PATH:$PWD/geckodriver
+tar -xvzf geckodriver-v0.15.0-linux64.tar.gz -C /usr/local/bin/
+sudo chmod +x /usr/local/bin/geckodriver
+geckodriver -V
 
 echo "---> Launching Selenium-Server-Standalone..."
 xvfb-run --server-args='-screen 0, 1024x768x16' java -jar /usr/bin/selenium.jar -port 4444 > /dev/null &
