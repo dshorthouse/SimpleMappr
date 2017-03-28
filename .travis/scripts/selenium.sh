96260819622455ee92fb60191e71ad4c89ace43f
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-28 10:23:46 -0400

    Oops in using geckodriver for Travis-CI selenium tests

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -4,8 +4,8 @@ wget "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standal
 sudo mv selenium-server-standalone-3.3.1.jar /usr/bin/selenium.jar
 
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz"
-tar -xzf geckodriver-v0.15.0-linux64.tar.gz
+sudo tar -xzf geckodriver-v0.15.0-linux64.tar.gz
 sudo mv geckodriver /usr/bin/geckodriver
 
 echo "---> Launching Selenium-Server-Standalone..."
-xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver -jar /usr/bin/selenium.jar -port 4444 > /dev/null &
+xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.gecko.driver=/usr/bin/geckodriver -jar /usr/bin/selenium.jar -port 4444 > /dev/null &
