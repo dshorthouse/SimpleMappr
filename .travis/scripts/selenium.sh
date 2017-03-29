7a0a6f06cdf12c3fd9cff6abbf3740161c652042
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-29 08:36:04 -0400

    Test connection to Selenium for Travis-CI

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -4,16 +4,17 @@ wget "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standal
 sudo mv selenium-server-standalone-3.3.1.jar /usr/local/bin/selenium.jar
 
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz"
-sudo tar -xvzf geckodriver-v0.15.0-linux64.tar.gz
+sudo tar -xzf geckodriver-v0.15.0-linux64.tar.gz
 sudo mv geckodriver /usr/local/bin/
 sudo chmod +x /usr/local/bin/geckodriver
 
 echo "---> Launching Selenium-Server-Standalone..."
-sudo xvfb-run --server-args='-screen 0, 1024x768x16' java -jar selenium.jar -port 4444 > /dev/null &
+sudo xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver -jar /usr/local/bin/selenium.jar -port 4444 > /dev/null &
 
-wget --retry-connrefused --tries=60 --waitretry=1 --output-file=/dev/null http://localhost:4444/wd/hub/status -O /dev/null
+echo "--> Test connection to Selenium..."
+wget --retry-connrefused --tries=5 --waitretry=1 --output-file=/dev/null http://localhost:4444/wd/hub/status -O /dev/null
 if [ ! $? -eq 0 ]; then
-    echo "Selenium Server not started"
+    echo "$(tput bold ; tput setaf 1)Selenium NOT running$(tput sgr0)"
 else
-    echo "Finished setup"
+    echo "$(tput bold ; tput setaf 2)Selenium running$(tput sgr0)"
 fi
