8d0ed9921470ee08642d3587cc66470bb21d468d
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-29 11:35:37 -0400

    Print java version

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -8,5 +8,7 @@ sudo tar -xzf geckodriver-v0.15.0-linux64.tar.gz
 sudo mv geckodriver /usr/local/bin/
 sudo chmod +x /usr/local/bin/geckodriver
 
+java -version
+
 echo "---> Launching Selenium-Server-Standalone..."
 sudo xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver -jar /usr/local/bin/selenium.jar -port 4444 > /dev/null &
