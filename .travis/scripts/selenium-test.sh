0e0d466cdab8520f60e30dd51d4df9f92415c53d
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-29 19:52:03 -0400

    Try wget for main page in Travis selenium testing

--- .travis/scripts/selenium-test.sh
+++ .travis/scripts/selenium-test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-wget -qO- "http://www.simplemappr.local"
-
 echo "---> Test connection to Selenium..."
 wget --retry-connrefused --tries=5 --waitretry=1 --output-file=/dev/null --output-document=/dev/null "http://localhost:4444/wd/hub/status"
 if [ ! $? -eq 0 ]; then
