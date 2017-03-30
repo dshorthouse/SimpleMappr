8eb03c04dacbbb487c36e6b61c2a9f3e0a58401f
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-30 10:07:09 -0400

    Revert to FF/geckodriver

--- .travis/scripts/selenium-test.sh
+++ .travis/scripts/selenium-test.sh
@@ -3,6 +3,7 @@
 wget -qO- "http://www.simplemappr.local"
 
 echo "---> Test connection to Selenium..."
+
 wget --retry-connrefused --tries=5 --waitretry=1 --output-file=/dev/null --output-document=/dev/null "http://localhost:4444/wd/hub/status"
 if [ ! $? -eq 0 ]; then
     echo "$(tput bold ; tput setaf 1)Selenium NOT running$(tput sgr0)"
--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -3,11 +3,11 @@
 wget "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standalone-3.3.1.jar"
 sudo mv selenium-server-standalone-3.3.1.jar /usr/local/bin/selenium.jar
 
-wget "https://chromedriver.storage.googleapis.com/2.28/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
-sudo chmod +x chromedriver
-sudo mv chromedriver /usr/local/bin
+wget "https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz"
+sudo tar -xzf geckodriver-v0.15.0-linux64.tar.gz
+sudo chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
 
 echo "---> Launching Selenium-Server-Standalone..."
 
-sudo xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium.jar -port 4444 > /dev/null &
+sudo xvfb-run --server-args='-screen 0, 1024x768x16' java -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver -jar /usr/local/bin/selenium.jar -port 4444 > /dev/null &
