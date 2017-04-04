79be33c907f19f08e44b02341e751f577fe4d43d
David Shorthouse <davidpshorthouse@gmail.com>
2017-04-04 15:45:02 -0400

    Zip, Tar - same thing, right?

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -4,7 +4,7 @@ wget "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standal
 sudo mv selenium-server-standalone-3.3.1.jar /usr/local/bin/selenium.jar
 
 wget "https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip"
-sudo tar -xzf chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
 sudo mv chromedriver /usr/local/bin/
 export PATH=$PATH:/usr/local/bin/chromedriver
