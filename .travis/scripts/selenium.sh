6768d72c34ecbae54c8799697ddced18e83132e5
David Shorthouse <davidpshorthouse@gmail.com>
2017-04-06 10:18:27 -0400

    Clean-up xvfb for chrome on Travis

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -6,18 +6,10 @@ sudo mv selenium-server-standalone-3.3.1.jar /usr/local/bin/selenium.jar
 wget "https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip"
 sudo unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
-sudo mv chromedriver /usr/local/bin/
+sudo mv chromedriver /usr/local/bin/chromedriver
 export PATH=$PATH:/usr/local/bin/chromedriver
-
-#wget "https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz"
-#sudo tar -xzf geckodriver-v0.15.0-linux64.tar.gz
-#sudo chmod +x geckodriver
-#sudo mv geckodriver /usr/local/bin/
+export CHROME_BIN=/usr/bin/google-chrome
 
 echo "---> Launching Selenium-Server-Standalone..."
 
-export CHROME_BIN=/usr/bin/google-chrome
-
-sudo xvfb-run --error-file="$TRAVIS_BUILD_DIR/xvfb-error.log" --server-args='-screen 0, 1024x768x16' java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -Dwebdriver.chrome.logfile=$TRAVIS_BUILD_DIR/chrome.log -jar /usr/local/bin/selenium.jar -port 4444 -log $TRAVIS_BUILD_DIR//selenium.log > /dev/null &
-
-#sudo xvfb-run --error-file="$TRAVIS_BUILD_DIR/xvfb-error.log" --server-args="-screen 0, 1024x768x16" java -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver -jar /usr/local/bin/selenium.jar -port 4444 > /dev/null &
+sudo xvfb-run --error-file="$TRAVIS_BUILD_DIR/xvfb-error.log" --server-args='-screen 0, 1024x768x16' java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -Dwebdriver.chrome.logfile=$TRAVIS_BUILD_DIR/chrome.log -jar /usr/local/bin/selenium.jar -port 4444 -log $TRAVIS_BUILD_DIR/selenium.log > /dev/null &
