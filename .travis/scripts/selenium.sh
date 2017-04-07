76165b9a43e59f548d40c5fcdf36b3eebba22d7c
David Shorthouse <davidpshorthouse@gmail.com>
2017-04-07 13:13:16 -0400

    Try ENV setting to get Chrome to launch in Travis

--- .travis/scripts/selenium.sh
+++ .travis/scripts/selenium.sh
@@ -12,4 +12,4 @@ export CHROME_BIN=/usr/bin/google-chrome
 
 echo "---> Launching Selenium-Server-Standalone..."
 
-sudo xvfb-run --error-file="$TRAVIS_BUILD_DIR/xvfb-error.log" --server-args='-screen 0, 1024x768x16' java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -Dwebdriver.chrome.logfile=$TRAVIS_BUILD_DIR/chrome.log -jar /usr/local/bin/selenium.jar -port 4444 -log $TRAVIS_BUILD_DIR/selenium.log > /dev/null &
+sudo xvfb-run --error-file="$TRAVIS_BUILD_DIR/xvfb-error.log" --server-args="-screen 0 1024x768x24" java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -Dwebdriver.chrome.logfile=$TRAVIS_BUILD_DIR/chrome.log -jar /usr/local/bin/selenium.jar -port 4444 -log $TRAVIS_BUILD_DIR/selenium.log > /dev/null &
