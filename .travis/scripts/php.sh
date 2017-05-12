57a6045d00a1859e46bd1c7ae54d060868b9fede
David Shorthouse <davidpshorthouse@gmail.com>
2017-05-12 10:19:30 -0400

    Fix multiline package list in apt-get command for Travis

--- .travis/scripts/php.sh
+++ .travis/scripts/php.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-packagelist=(
-  php5-cli
-  php5-common
-  php5-curl
-  php5-dev
-  php5-fpm
-  php5-gd
-  php5-imagick
-  php5-mysql
-)
-
-sudo apt-get install ${packagelist[@]}
+sudo apt-get install \
+php5-cli \
+php5-common \
+php5-curl \
+php5-dev \
+php5-fpm \
+php5-gd \
+php5-imagick \
+php5-mysql
