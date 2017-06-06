5f68790d302ca7d3da3bbe2f71d8c4494312231b
David Shorthouse <davidpshorthouse@gmail.com>
2017-06-06 09:30:41 -0400

    Re-enable all tests for Travis

--- Tests/SimpleMapprTest.php
+++ Tests/SimpleMapprTest.php
@@ -570,7 +570,6 @@ abstract class SimpleMapprTest extends TestCase
         $image1 = new \Imagick($fn1);
         $image2 = new \Imagick($fn2);
         $result = $image1->compareImages($image2, \Imagick::METRIC_MEANSQUAREERROR);
-        echo $result[1];
         if ($result[1] < 0.01) {
             $similar = true;
         }
