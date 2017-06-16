7cfc58302cfdea4fbc359825f4f663256ab23dee
David Shorthouse <davidpshorthouse@gmail.com>
2017-06-16 08:41:50 -0400

    Use ENV variable for servername in apache config files

--- .travis/scripts/apache-vhosts.sh
+++ .travis/scripts/apache-vhosts.sh
@@ -4,10 +4,12 @@ echo "---> Starting $(tput bold ; tput setaf 2)virtual host creation$(tput sgr0)
 
 sudo cp -f .travis/apache2/www_simplemappr_test.conf /etc/apache2/sites-available/www_simplemappr_test.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/www_simplemappr_test.conf
+sudo sed -e "s?%HOST%?$HOST?g" --in-place /etc/apache2/sites-available/www_simplemappr_test.conf
 sudo a2ensite www_simplemappr_test.conf
 
 sudo cp -f .travis/apache2/img_simplemappr_test.conf /etc/apache2/sites-available/img_simplemappr_test.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/img_simplemappr_test.conf
+sudo sed -e "s?%IMG_HOST%?$IMG_HOST?g" --in-place /etc/apache2/sites-available/www_simplemappr_test.conf
 sudo a2ensite img_simplemappr_test.conf
 
 sudo a2enmod actions alias fastcgi rewrite expires headers vhost_alias
