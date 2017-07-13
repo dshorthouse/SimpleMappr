a281a213c2ea6f7d8b9a95d2dd39843a0fe6b4dd
David Shorthouse <davidpshorthouse@gmail.com>
2017-07-13 15:02:38 -0400

    Increase timeout for cURL tests

--- Tests/SimpleMapprTestMixin.php
+++ Tests/SimpleMapprTestMixin.php
@@ -108,6 +108,8 @@ trait SimpleMapprTestMixin
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 400);
         curl_setopt($ch, CURLOPT_URL, $url);
 
         switch($type) {
