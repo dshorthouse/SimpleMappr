c9220d60b17238f7685070783bf6cef3d039d94b
David Shorthouse <davidpshorthouse@gmail.com>
2018-07-30 13:18:28 -0400

    Yet again...

--- .travis/scripts/mapserver.sh
+++ .travis/scripts/mapserver.sh
@@ -8,7 +8,7 @@ wget http://download.osgeo.org/mapserver/$mapserver.tar.gz
 tar -zxvf $mapserver.tar.gz
 mkdir $mapserver/build
 cd $mapserver/build
-cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=1 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 ..
+cmake -DWITH_KML=1 -DWITH_PHP=1 -DWITH_FCGI=1 -DWITH_SVGCAIRO=0 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 -DWITH_PROTOBUFC=0 ..
 make
 sudo make install
 cd ../../
